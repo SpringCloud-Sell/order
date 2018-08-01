8c27699e0cf5712fdb425a28daa39be77b1335a7
Rushing <liming20110711@163.com>
2018-08-01 08:25:37 +0800

    update:20180801-01

--- src/main/java/com/coding/order/controller/OrderController.java
+++ src/main/java/com/coding/order/controller/OrderController.java
@@ -12,15 +12,53 @@
  ********************************************************************************/
 package com.coding.order.controller;
 
+import com.coding.order.converter.OrderForm2OrderDTOConverter;
+import com.coding.order.dto.OrderDTO;
+import com.coding.order.enums.ResultEnum;
+import com.coding.order.exception.OrderException;
+import com.coding.order.form.OrderForm;
+import com.coding.order.service.OrderService;
+import com.coding.order.utils.ResultVOUtil;
+import com.coding.order.vo.ResultVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/order")
+@Slf4j
 public class OrderController {
 
-    public void create() {
+    @Autowired private OrderService orderService;
+
+    public ResultVO create(@Valid OrderForm orderForm, BindingResult bindingResult) {
         // 1.参数校验
+        if (bindingResult.hasErrors()) {
+            log.error("【创建订单】参数不正确，orderForm={}", orderForm);
+            throw new OrderException(
+                    ResultEnum.PARAM_ERROR.getCode(),
+                    bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
+            log.error("【创建订单】购物车信息为空");
+            throw new OrderException(ResultEnum.CART_EMPTY);
+        }
+
+        OrderDTO result = orderService.create(orderDTO);
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", result.getOrderId());
+
+        return ResultVOUtil.success(map);
+
         // 2.查询商品信息（调用商品服务）
         // 3.计算总价
         // 4.扣库存（调用商品服务)
--- src/main/java/com/coding/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/coding/order/service/impl/OrderServiceImpl.java
@@ -12,24 +12,44 @@
  ********************************************************************************/
 package com.coding.order.service.impl;
 
+import com.coding.order.dataobject.OrderMaster;
 import com.coding.order.dto.OrderDTO;
+import com.coding.order.enums.OrderStatusEnum;
+import com.coding.order.enums.PayStatusEnum;
 import com.coding.order.repository.OrderDetailRepository;
 import com.coding.order.repository.OrderMasterRepository;
 import com.coding.order.service.OrderService;
+import com.coding.order.utils.KeyUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    @Autowired private OrderDetailRepository orderDetailRepository;
+    @Autowired private OrderDetailRepository detailRepository;
 
-    @Autowired private OrderMasterRepository orderMasterRepository;
+    @Autowired private OrderMasterRepository masterRepository;
 
     @Override
     public OrderDTO create(OrderDTO orderDTO) {
         // TODO: 2018/7/31 查询商品信息（调用商品服务）
-        
-        return null;
+
+        // TODO: 2018/8/1 计算总价
+
+        // TODO: 2018/8/1 扣库存（调用商品服务）
+
+        // 订单入库
+        OrderMaster orderMaster = new OrderMaster();
+        orderDTO.setOrderId(KeyUtil.getUniqueKey());
+        BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderAmount(new BigDecimal(5));
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+
+        masterRepository.save(orderMaster);
+        return orderDTO;
     }
 }
